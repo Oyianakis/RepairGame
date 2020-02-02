2472cb78d8ddfea2e9cb625fedf611b7989c716f
James Cho <chojames500@gmail.com>
2020-02-02 03:24:18 -0600

    Objective system, spawning system, and scoring system now work with each other

---
+++ Real repair Game/Assets/Scripts/CustomScripts.CustomUI/ScoreBoard.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace CustomScripts.CustomUI
+{
+    public class ScoreBoard : MonoBehaviour
+    {
+        [SerializeField] private int _relatedPlayerID;
+        public int RelatedPlayerID { get => _relatedPlayerID; }
+        public TextMeshProUGUI ScoreText { get; private set; }
+        public int Score { get; private set; }
+
+        private void Start()
+        {
+            this.ScoreText = GetComponent<TextMeshProUGUI>();
+
+            this.ScoreText.text = $"Player {this.RelatedPlayerID}\n0";
+        }
+
+        public void UpdateScore()
+        {
+            this.Score++;
+            this.ScoreText.text = $"Player {this.RelatedPlayerID}\n{this.Score}";
+        }
+    }
+}
